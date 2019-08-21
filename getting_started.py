0f94cd6a34bf4109550a782f4b3be9417aa970ce
Arshad Ansari <arshadansari27@gmail.com>
2019-08-22 00:32:12 +0530

    Initial

--- getting_started.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")  # What's a decorator doing here. Is there an alternative
-def hello():
-    return "Hello World!"
-
-if __name__ == "__main__":
-    app.run()
